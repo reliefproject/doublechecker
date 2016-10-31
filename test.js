7fc0fd5dcf104136c2843aa15161fe7a12627515
toenu23 <fubarphukhew@gmail.com>
2016-10-31 14:45:15 +0100

    tcp transport

--- test.js
+++ test.js
@@ -2,9 +2,20 @@ const DoubleChecker = require('./index');
 
 let dc = new DoubleChecker({
   dataType: 'json',
-  ignoreJSONKeys: ['requestProcessingTime'],
+  //IgnoreJSONKeys: ['requestProcessingTime'],
+  ignoreJSONKeys: ['id'],
   sources: [
     {
+      transport: 'tcp',
+      host: 'electrum.online',
+      port: 50001,
+    },
+    {
+      transport: 'tcp',
+      host: 'electrum.jdubya.info',
+      port: 50001,
+    },
+/*    {
       transport: 'http',
       host: '23.94.134.161',
       port: 7876,
@@ -43,11 +54,12 @@ let dc = new DoubleChecker({
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-    },
+    },*/
   ],
 });
 
-dc.request({ requestType: 'getAccount', account: 'NXT-8SWM-2224-YKWW-22222' }, function(err, data) {
+//Dc.request({ requestType: 'getAccount', account: 'NXT-8SWM-2224-YKWW-22222' }, function(err, data) {
+dc.request('{"id": 1, "method":"blockchain.address.get_history", "params":["1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L"] }\n', function(err, data) {
   if (err) {
     return console.log(err);
   }
@@ -55,3 +67,15 @@ dc.request({ requestType: 'getAccount', account: 'NXT-8SWM-2224-YKWW-22222' }, f
   console.log('Freq ', data.frequency);
   console.log('Score ', data.score);
 });
+
+
+setTimeout(function() {
+  dc.request('{"id": 1, "method":"server.version", "params":[] }\n', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data)
+    console.log('Freq ', data.frequency);
+    console.log('Score ', data.score);
+  });
+}, 5000);
