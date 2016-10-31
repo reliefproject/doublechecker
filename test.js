e245225f6d6ead74939c68458168846c65608e65
toenu23 <fubarphukhew@gmail.com>
2016-10-31 14:13:42 +0100

    http transport

--- test.js
+++ test.js
@@ -8,34 +8,50 @@ let dc = new DoubleChecker({
       transport: 'http',
       host: '23.94.134.161',
       port: 7876,
-      pathname: '/nxt',
+      path: '/nxt',
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
     },
     {
       transport: 'http',
       host: 'nrs.scripterron.org',
       port: 7876,
-      pathname: '/nxt',
+      path: '/nxt',
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
     },
     {
       transport: 'http',
       host: 'nxt.notbot.me',
       port: 7876,
-      pathname: '/nxt',
+      path: '/nxt',
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
     },
     {
       transport: 'http',
       host: 'nxt1.scriba.io',
       port: 7876,
-      pathname: '/nxt',
+      path: '/nxt',
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
     },
   ],
 });
 
-dc.request('test', function(err, data) {
-  console.log(err);
-  console.log(data);
+dc.request({ requestType: 'getAccount', account: 'NXT-8SWM-2224-YKWW-22222' }, function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data)
+  console.log('Freq ', data.frequency);
+  console.log('Score ', data.score);
 });
