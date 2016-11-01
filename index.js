5bed59d361465d3fca2d70fc58a9d6cd8b854380
toenu23 <fubarphukhew@gmail.com>
2016-11-01 12:07:43 +0100

    closure

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-const SourceManager = require('./lib/source_manager')
-const Request = require('./lib/request')
+const DoubleChecker = (function() {
 
-let self;
+  const SourceManager = require('./lib/source_manager')
+  const Request = require('./lib/request')
 
-class DoubleChecker {
+  class DoubleChecker {
 
-  constructor(params) {
-    this.numUseSources = params.numUseSources
-      ? params.numUseSources
-      : 3;
-    this.dataType = ['string', 'json'].indexOf(params.dataType) !== -1
-      ? params.dataType
-      : 'string';
-    this.ignoreJSONKeys = params.ignoreJSONKeys instanceof Array
-      ? params.ignoreJSONKeys
-      : [];
-    this.sources = params.sources instanceof Array
-      ? params.sources
-      : [];
-    this.sourceManager = new SourceManager(this.sources);
-    self = this;
+    constructor(params) {
+      this.numUseSources = params.numUseSources
+        ? params.numUseSources
+        : 3;
+      this.dataType = ['string', 'json'].indexOf(params.dataType) !== -1
+        ? params.dataType
+        : 'string';
+      this.ignoreJSONKeys = params.ignoreJSONKeys instanceof Array
+        ? params.ignoreJSONKeys
+        : [];
+      this.sources = params.sources instanceof Array
+        ? params.sources
+        : [];
+      this.sourceManager = new SourceManager(this.sources);
+    };
+
+
+    request(data, callback) {
+      if (typeof callback !== 'function') {
+        return console.log(
+          new Error('Callback not specified')
+        );
+      }
+      const now = new Date();
+      const startTime = now.getTime();
+      let req = new Request({
+        params: {
+          dataType: this.dataType,
+          ignoreJSONKeys: this.ignoreJSONKeys,
+          callback: callback,
+          startTime: startTime,
+        },
+        sources: this.sourceManager.selectSources(this.numUseSources),
+        sourceManager: this.sourceManager,
+        data: data,
+      });
+      req.execute();
+    };
   };
 
-  request(data, callback) {
-    if (typeof callback !== 'function') {
-      return console.log(
-        new Error('Callback not specified')
-      );
-    }
-    const now = new Date();
-    const startTime = now.getTime();
-    let req = new Request({
-      params: {
-        dataType: self.dataType,
-        ignoreJSONKeys: self.ignoreJSONKeys,
-        callback: callback,
-        startTime: startTime,
-      },
-      sources: self.sourceManager.selectSources(self.numUseSources),
-      sourceManager: self.sourceManager,
-      data: data,
-    });
-    req.execute();
-  };
-};
+  return DoubleChecker;
+
+})();
 
 module.exports = DoubleChecker;
